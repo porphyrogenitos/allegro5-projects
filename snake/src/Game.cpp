47f60c74899761d99648de581d13639ad4bf77e4
alvaromichael123@gmail.com <alvaromichael123@gmail.com>
2024-01-17 09:02:40 -0600

    Removed uses of TileGrid class.

--- snake/src/Game.cpp
+++ snake/src/Game.cpp
@@ -10,34 +10,6 @@ const int DISP_WIDTH = 640;
 const int DISP_HEIGHT = 480;
 const int TILE_WIDTH = 20;
 
-void tilegrid_update_snake(Snake snake, TileGrid& tilegrid) {
-
-    tilegrid.update_tile(snake.get_head_r(), snake.get_head_c(), Tile::snake_head);
-
-    int cur_row = snake.get_head_r();
-    int cur_col = snake.get_head_c();
-
-    for (int index = 1; index < snake.get_length(); index++) {
-        Direction pos = snake.get_segment_position(index);
-        switch (pos) {
-            case Direction::north:
-                cur_row -= 1;
-                break;
-            case Direction::south:
-                cur_row += 1;
-                break;
-            case Direction::east:
-                cur_col += 1;
-                break;
-            case Direction::west:
-                cur_col -= 1;
-                break;
-            default:
-                break;
-        }
-        tilegrid.update_tile(cur_row, cur_col, Tile::snake_body);
-    }
-}
 
 void clear_snake(Snake snake) {
 
@@ -69,36 +41,6 @@ void clear_snake(Snake snake) {
         int y2 = (cur_row + 1) * TILE_WIDTH;
 
         al_draw_filled_rectangle(x1, y1, x2, y2, al_map_rgb(0, 0, 0));
-    }
-}
-
-void clear_snake_from_tilegrid(Snake snake, TileGrid& tilegrid) {
-
-    int cur_row = snake.get_head_r();
-    int cur_col = snake.get_head_c();
-
-    tilegrid.update_tile(snake.get_head_r(), snake.get_head_c(), Tile::empty);
-
-    for (int index = 1; index < snake.get_length(); index++) {
-        Direction pos = snake.get_segment_position(index);
-        switch (pos) {
-            case Direction::north:
-                cur_row -= 1;
-                break;
-            case Direction::south:
-                cur_row += 1;
-                break;
-            case Direction::east:
-                cur_col += 1;
-                break;
-            case Direction::west:
-                cur_col -= 1;
-                break;
-            default:
-                break;
-        }
-
-        tilegrid.update_tile(cur_row, cur_col, Tile::empty);
     }
 }
 
@@ -146,21 +88,19 @@ void draw_snake(Snake snake) {
         int center_y = (y1 + y2) / 2;
         float rad = TILE_WIDTH / 2;
 
-        //tilegrid.update_tile(cur_row, cur_col, Tile::empty);
         al_draw_filled_circle(center_x, center_y, rad, al_map_rgb(0, 255, 0));
     }
 }
 
-void draw_grid(TileGrid& tilegrid) {
-    for (int row = 0; row < tilegrid.num_rows; row++) {
-        for (int col = 0; col < tilegrid.num_cols; col++) {
-            int x1 = col * TILE_WIDTH;
-            int y1 = row * TILE_WIDTH;
-            int x2 = (col + 1) * TILE_WIDTH;
-            int y2 = (row + 1) * TILE_WIDTH;
+void draw_grid(int num_rows, int num_cols) {
+    for (int row = 0; row < num_rows; row++) {
+        int y = row * TILE_WIDTH;
+        al_draw_line(0, y, DISP_WIDTH, y, al_map_rgb(255, 0, 0), 1.0);
+    }
 
-            al_draw_rectangle(x1, y1, x2, y2, al_map_rgb(255, 0, 0), 1); //Temporary: Just to help verify.
-        }
+    for (int col = 0; col < num_cols; col++) {
+        int x = col * TILE_WIDTH;
+        al_draw_line(x, 0, x, DISP_HEIGHT, al_map_rgb(255, 0, 0), 1.0);
     }
 }
 
@@ -190,17 +130,17 @@ int main() {
     al_start_timer(timer);
 
     TileGrid tilegrid {DISP_WIDTH, DISP_HEIGHT, TILE_WIDTH};
+
+    int tilegrid_num_rows = DISP_HEIGHT / TILE_WIDTH;
+    int tilegrid_num_cols = DISP_WIDTH / TILE_WIDTH;
     
 
     Snake snake {Direction::east, 10, 10};
     snake.update_head_dir(Direction::north);
     snake.print();
 
-    //draw_snake(snake, head_x, head_y);
-    tilegrid_update_snake(snake, tilegrid);
-    draw_snake(snake, tilegrid);
-    draw_grid(tilegrid);
-    std::cout << tilegrid.to_string();
+    draw_snake(snake);
+    draw_grid(tilegrid_num_rows, tilegrid_num_cols);
     al_flip_display();
 
 
@@ -225,14 +165,11 @@ int main() {
 
         if (redraw && al_event_queue_is_empty(event_queue)) {
             clear_snake(snake);
-            clear_snake_from_tilegrid(snake, tilegrid);
             snake.move();
 
-            tilegrid_update_snake(snake, tilegrid);
             draw_snake(snake);
-            std::cout << tilegrid.to_string() << "\n";
 
-            draw_grid(tilegrid);
+            draw_grid(tilegrid_num_rows, tilegrid_num_cols);
 
             al_flip_display();
             
