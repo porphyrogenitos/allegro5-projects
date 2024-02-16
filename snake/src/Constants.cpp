f6208175fe97013800721bd7989f2db63d82c0e4
alvaromichael123@gmail.com <alvaromichael123@gmail.com>
2024-02-16 08:45:49 -0600

    Changed constants to constexpr

--- snake/src/Constants.cpp
+++ snake/src/Constants.cpp
@@ -1,9 +1,9 @@
-extern const int DISP_WIDTH = 640;
-extern const int DISP_HEIGHT = 480;
-extern const int TILE_WIDTH = 20;
- 
-extern const int KEY_SEEN = 1;
-extern const int KEY_RELEASED = 2;
- 
-extern const int tilegrid_num_rows = DISP_HEIGHT / TILE_WIDTH;
-extern const int tilegrid_num_cols = DISP_WIDTH / TILE_WIDTH;
+extern constexpr int DISP_WIDTH = 640;
+extern constexpr int DISP_HEIGHT = 480;
+extern constexpr int TILE_WIDTH = 20;
+
+extern constexpr int KEY_SEEN = 1;
+extern constexpr int KEY_RELEASED = 2;
+
+extern constexpr int tilegrid_num_rows = DISP_HEIGHT / TILE_WIDTH;
+extern constexpr int tilegrid_num_cols = DISP_WIDTH / TILE_WIDTH;
