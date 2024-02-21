8dfca37a62da4c6f283e8e491c293cb97b08fa79
alvaromichael123@gmail.com <alvaromichael123@gmail.com>
2024-02-21 11:47:19 -0600

    Added alignment flags to Label

--- snake/src/Label.cpp
+++ snake/src/Label.cpp
@@ -1,6 +1,6 @@
 #include "Label.hpp"
 
-Label::Label(int x, int y, int width, int height, std::string text, ALLEGRO_COLOR text_color, ALLEGRO_FONT* font) {
+Label::Label(int x, int y, int width, int height, std::string text, ALLEGRO_COLOR text_color, ALLEGRO_FONT* font, int alignment_flag) {
     this->x = x;
     this->y = y;
     this->width = width;
@@ -8,10 +8,24 @@ Label::Label(int x, int y, int width, int height, std::string text, ALLEGRO_COLO
     this->text = text;
     this->text_color = text_color;
     this->font = font;
+    this->alignment_flag = alignment_flag;
 }
 
 void Label::draw() {
-    al_draw_text(font, text_color, get_center_x(), get_center_y(), ALLEGRO_ALIGN_CENTRE, text.c_str());
+    switch (alignment_flag)
+    {
+    case ALLEGRO_ALIGN_CENTRE:
+        al_draw_text(font, text_color, get_center_x(), get_center_y(), ALLEGRO_ALIGN_CENTRE, text.c_str());
+        break;
+    case ALLEGRO_ALIGN_LEFT:
+        al_draw_text(font, text_color, x, y, ALLEGRO_ALIGN_LEFT, text.c_str());
+        break;
+    case ALLEGRO_ALIGN_RIGHT:
+        al_draw_text(font, text_color, x + width, y + height, ALLEGRO_ALIGN_RIGHT, text.c_str());
+    
+    default:
+        break;
+    }
 }
 
 // Gets the center x-coordinate of the Label.
--- snake/src/Label.hpp
+++ snake/src/Label.hpp
@@ -14,8 +14,9 @@ public:
     int height;
     ALLEGRO_FONT* font;
     ALLEGRO_COLOR text_color;
+    int alignment_flag;
 
-    Label(int x, int y, int width, int height, std::string text, ALLEGRO_COLOR text_color, ALLEGRO_FONT* font);
+    Label(int x, int y, int width, int height, std::string text, ALLEGRO_COLOR text_color, ALLEGRO_FONT* font, int alignment_flag);
 
     void draw();
 
